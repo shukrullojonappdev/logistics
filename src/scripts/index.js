3de79b799c69d1ddf1622a0982d94e639f58f0c6
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-07-25 11:15:48 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,25 +3,26 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('#bg_image', {
+gsap.to("#bg_image", {
   scrollTrigger: {
-    trigger: '#gsap_1',
-    start: 'top bottom',
-    end: 'bottom top',
+    trigger: "#gsap_1",
+    start: "top bottom",
+    end: "bottom top",
     scrub: true,
+    markers: true,
   },
-  y: window.innerHeight * 0.75,
-  ease: 'none',
+  translateY: window.innerHeight * 0.5,
+  ease: "none",
   duration: 1,
-})
+});
 
-gsap.to('#gsap_2', {
+gsap.to("#gsap_2", {
   scrollTrigger: {
-    trigger: '#gsap_2',
-    start: 'top center',
-    end: 'bottom top',
+    trigger: "#gsap_2",
+    start: "top center",
+    end: "bottom top",
   },
   opacity: 1,
   y: 16,
-  duration: 1
-})
+  duration: 1,
+});
