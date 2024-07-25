29f58b5d9f1548ad491bb881bc41281cb330ab38
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-07-25 15:41:36 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,18 +10,7 @@ gsap.to("#bg_image", {
     end: "bottom top",
     scrub: true,
   },
-  y: window.innerHeight * 0.5,
+  y: -window.innerHeight * 0.5,
   ease: "none",
-  duration: 1,
-});
-
-gsap.to("#gsap_2", {
-  scrollTrigger: {
-    trigger: "#gsap_2",
-    start: "top center",
-    end: "bottom top",
-  },
-  opacity: 1,
-  y: 16,
   duration: 1,
 });
