97ab9c262afaa5ddf6803a500186390bc5e47b11
shukrullojonappdev <shukrullojon.dev@gmail.com>
2024-07-27 22:50:40 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,51 +7,46 @@ import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
 
 gsap.registerPlugin(ScrollTrigger);
-
-gsap.set("#bg_image", { y: 0 });
-gsap.to("#bg_image", {
-  scrollTrigger: {
-    trigger: "#gsap_1",
-    start: "top bottom",
-    end: "bottom top",
-    scrub: true,
-    markers: true
-  },
-  y: window.innerHeight * 0.5,
-  ease: "none",
-  duration: 1,
-  immediateRender: true,
-  overwrite: true
-});
-
-gsap.utils.toArray(".img").forEach((img, i) => {
-  gsap.set(img, { y: 0 });
-  gsap.to(img, {
+(() => {
+  gsap.set("#bg_image", { y: 0 });
+  gsap.to("#bg_image", {
     scrollTrigger: {
-      trigger: `.parallax_${i + 1}`,
+      trigger: "#gsap_1",
       start: "top bottom",
       end: "bottom top",
-      scrub: true,
-      markers: true
+      scrub: 0.1,
     },
-    y: -400,
-    ease: "none",
-    duration: 1,
+    y: window.innerHeight * 0.5,
     immediateRender: true,
     overwrite: true
   });
-});
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 15,
-  modules: [Navigation, Pagination],
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+  gsap.utils.toArray(".img").forEach((img, i) => {
+    gsap.set(img, { top: 0 });
+    gsap.to(img, {
+      scrollTrigger: {
+        trigger: `.parallax_${i + 1}`,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.3,
+      },
+      top: -400,
+      immediateRender: true,
+      overwrite: true
+    });
+  });
+
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    modules: [Navigation, Pagination],
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+})()
