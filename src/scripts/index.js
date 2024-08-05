5ad49ee91471a63cfbee9032bbb691bc6a231367
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-08-05 11:12:04 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,6 +21,25 @@ window.addEventListener("load", () => {
     });
   });
 
+  gsap.utils.toArray('.stat').forEach((stat, i) => {
+    gsap.to(stat, {
+      scrollTrigger: {
+        trigger: `#stat${i+1}`,
+        start: 'top bottom',
+        end: 'bottom top',
+        onEnter: () => {
+          switch (i) {
+            case 0: animateValue('stat1', 0, 10, 1500); break;
+            case 1: animateValue('stat2', 0, 24, 1500); break;
+            case 2: animateValue('stat3', 0, 150, 1500); break;
+            case 3: animateValue('stat4', 0, 300, 1500); break;
+            default: return
+          }
+        },
+      }
+    })
+  })
+
   const swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 15,
@@ -35,3 +54,19 @@ window.addEventListener("load", () => {
     },
   });
 })
+
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
