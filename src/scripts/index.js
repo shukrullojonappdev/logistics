30c22f7e1585471dd4f24bcd5c2b38e02c2d0da4
shukrullojonappdev <shukrullojon.dev@gmail.com>
2024-08-10 23:17:48 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,21 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
+import Lenis from 'lenis'
+
+const lenis = new Lenis()
+
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 gsap.registerPlugin(ScrollTrigger);
+lenis.on('scroll', ScrollTrigger.update)
+
 window.addEventListener("load", () => {
   gsap.utils.toArray(".img").forEach((img, i) => {
     gsap.to(img, {
@@ -25,7 +38,7 @@ window.addEventListener("load", () => {
   gsap.utils.toArray('.stat').forEach((stat, i) => {
     gsap.to(stat, {
       scrollTrigger: {
-        trigger: `#stat${i+1}`,
+        trigger: `#stat${i + 1}`,
         start: 'top bottom',
         end: 'bottom top',
         onEnter: () => {
@@ -38,6 +51,21 @@ window.addEventListener("load", () => {
           }
         },
       }
+    })
+  })
+
+  gsap.utils.toArray('.about_text').forEach((text, i) => {
+    gsap.to(text, {
+      scrollTrigger: {
+        trigger: `.about_text_trigger_${i + 1}`,
+        start: `top bottom`,
+        end: 'bottom top',
+      },
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: 'power4.in',
+      overwrite: true
     })
   })
 
@@ -60,14 +88,14 @@ function animateValue(id, start, end, duration) {
   if (start === end) return;
   var range = end - start;
   var current = start;
-  var increment = end > start? 1 : -1;
+  var increment = end > start ? 1 : -1;
   var stepTime = Math.abs(Math.floor(duration / range));
   var obj = document.getElementById(id);
-  var timer = setInterval(function() {
-      current += increment;
-      obj.innerHTML = current;
-      if (current == end) {
-          clearInterval(timer);
-      }
+  var timer = setInterval(function () {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
   }, stepTime);
 }
