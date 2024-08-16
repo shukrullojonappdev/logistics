56478235f6608af077473302b62b86b9ff8bdc7b
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-08-16 16:38:32 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -49,40 +49,69 @@ window.addEventListener("load", () => {
     }
   });
 
-  if (headerLinksEl.length > 0) {
-    for (let i = 0; i < headerLinksEl.length; i++) {
-      headerLinksEl[i].addEventListener("click", (e) => {
-        if (window.innerWidth <= 800) {
-          document.body.style.overflowY = "auto";
-          lenis.start();
-          headerNav.style.right = "800px";
-          headerList.style.left = "-300px";
-        }
-        lenis.scrollTo(`#${links[i]}`, {
-          duration: i + 1,
-          easing: (x) => {
-            return 1 - (1 - x) * (1 - x);
-          },
-        });
-      });
-    }
-  }
+  // if (headerLinksEl.length > 0) {
+  //   for (let i = 0; i < headerLinksEl.length; i++) {
+  //     headerLinksEl[i].addEventListener("click", (e) => {
+  //       if (window.innerWidth <= 800) {
+  //         document.body.style.overflowY = "auto";
+  //         lenis.start();
+  //         headerNav.style.right = "800px";
+  //         headerList.style.left = "-300px";
+  //       }
+  // lenis.scrollTo(`#${links[i]}`, {
+  //   duration: i + 1,
+  //   easing: (x) => {
+  //     return 1 - (1 - x) * (1 - x);
+  //   },
+  // });
+  //     });
+  //   }
+  // }
 
-  const footer = document.getElementsByTagName("footer")[0];
-  const footerLinksEl = footer.getElementsByTagName("li");
+  // const footer = document.getElementsByTagName("footer")[0];
+  // const footerLinksEl = footer.getElementsByTagName("li");
 
-  if (footerLinksEl.length > 0) {
-    for (let i = 0; i < footerLinksEl.length; i++) {
-      footerLinksEl[i].addEventListener("click", (e) => {
-        lenis.scrollTo(`#${links[i]}`, {
-          duration: footerLinksEl.length - i - 1,
-          easing: (x) => {
-            return 1 - (1 - x) * (1 - x);
-          },
-        });
-      });
-    }
-  }
+  // if (footerLinksEl.length > 0) {
+  //   for (let i = 0; i < footerLinksEl.length; i++) {
+  //     footerLinksEl[i].addEventListener("click", (e) => {
+  //       lenis.scrollTo(`#${links[i]}`, {
+  //         duration: footerLinksEl.length - i - 1,
+  //         easing: (x) => {
+  //           return 1 - (1 - x) * (1 - x);
+  //         },
+  //       });
+  //     });
+  //   }
+  // }
+
+  gsap.utils.toArray(".stat").forEach((stat, i) => {
+    gsap.to(stat, {
+      scrollTrigger: {
+        trigger: `#stat${i + 1}`,
+        trigger: `#stat${i + 1}`,
+        start: "top bottom",
+        end: "bottom top",
+        onEnter: () => {
+          switch (i) {
+            case 0:
+              animateValue("stat1", 0, 10, 1500);
+              break;
+            case 1:
+              animateValue("stat2", 0, 24, 1500);
+              break;
+            case 2:
+              animateValue("stat3", 0, 150, 1500);
+              break;
+            case 3:
+              animateValue("stat4", 0, 300, 1500);
+              break;
+            default:
+              return;
+          }
+        },
+      },
+    });
+  });
 
   gsap.utils.toArray(".text-animation").forEach((text, i) => {
     gsap.to(text, {
@@ -113,3 +142,21 @@ window.addEventListener("load", () => {
     },
   });
 });
+
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start ? 1 : -1;
+  var increment = end > start ? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+
+  var timer = setInterval(function () {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
+}
