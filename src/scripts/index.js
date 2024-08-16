d24a8d6c36762d8d1cf40f37d3a17d090f47a615
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-08-16 09:00:44 +0500

    commited

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,11 +22,42 @@ lenis.on("scroll", ScrollTrigger.update);
 window.addEventListener("load", () => {
   const header = document.getElementsByTagName("header")[0];
   const headerLinksEl = header.getElementsByTagName("li");
+  const headerBurger = header.getElementsByTagName("i")[0];
+  const headerNav = header.getElementsByTagName("nav")[0];
+  const headerList = header.getElementsByTagName("ul")[0];
   const links = ["about", "driver", "broker", "job", "contact"];
+
+  headerBurger.addEventListener("click", () => {
+    if (window.innerWidth <= 800) {
+      document.body.style.overflowY = "hidden";
+      lenis.stop();
+      headerNav.style.right = 0;
+      headerList.style.left = 0;
+    }
+  });
+
+  headerNav.addEventListener("click", (e) => {
+    if (
+      e.target.nodeName != "UL" &&
+      e.target.nodeName != "LI" &&
+      window.innerWidth <= 800
+    ) {
+      document.body.style.overflowY = "auto";
+      lenis.start();
+      headerNav.style.right = "800px";
+      headerList.style.left = "-300px";
+    }
+  });
 
   if (headerLinksEl.length > 0) {
     for (let i = 0; i < headerLinksEl.length; i++) {
       headerLinksEl[i].addEventListener("click", (e) => {
+        if (window.innerWidth <= 800) {
+          document.body.style.overflowY = "auto";
+          lenis.start();
+          headerNav.style.right = "800px";
+          headerList.style.left = "-300px";
+        }
         lenis.scrollTo(`#${links[i]}`, {
           duration: i + 1,
           easing: (x) => {
@@ -62,7 +93,7 @@ window.addEventListener("load", () => {
       },
       opacity: 1,
       y: 0,
-      duration: 1,
+      duration: 0.75,
       ease: "power4.in",
       overwrite: true,
     });
